5e967773a6f46951a159d1947e0806b51fb52c0d
sahar <saharabdalazeez97@gmail.com>
2022-11-25 21:43:03 +0200

    display the headre in app.js

--- ecommerce-product/src/App.js
+++ ecommerce-product/src/App.js
@@ -1,10 +1,14 @@
 import logo from "./logo.svg";
 import { Home } from "./containers/Home";
+import { Header } from "./components/Header.jsx";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+
       <Home />
     </div>
   );

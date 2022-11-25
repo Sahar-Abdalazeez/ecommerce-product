420b923454b26b7fa97abcc72d4dd03d07f9536e
sahar <saharabdalazeez97@gmail.com>
2022-11-25 16:35:16 +0200

    Gallery component

---
+++ ecommerce-product/src/components/Gallary.jsx
@@ -0,0 +1,92 @@
+import { useState } from 'react';
+import styled, { css } from 'styled-components';
+
+const MainGalleryContainer = styled.div`
+  display: flex;
+  flex:1;
+  width: 50%;
+  height: 100%;
+  flex-direction: column;
+`;
+const MainImage = styled.img`
+  height: 400px;
+  width: 45%;
+  min-width: 300px;
+  border-width: 2px;
+  border-radius: 10px;
+  margin-bottom: 40px;
+`;
+
+const ImageOptions = styled.div`
+display: flex;
+flex-direction:row;
+height:100px;
+width: 45%;
+min-width: 300px;
+justify-content:space-between ;
+align-items: center;
+`;
+
+const OptionContainer = styled.div`
+  width: 80px;
+  height:80px;
+  background-color: yellow;
+  border:${props => props.isSelected ? `solid 2px  hsl(26, 100%, 55%)` : `solid 2px transparnt`};
+  border-radius: 10px;
+  overflow: hidden
+`;
+
+const Option = styled.button`
+  background-color: transparent
+`;
+
+const OptionImage = styled.img`
+ width: 80px;
+  height:80px;
+  /* border-width: 5px;
+    border-radius: 10px;
+  border-color: orange;  */
+`;
+
+export const Gallery = ({ optionImages }) => {
+  const [mainSource, setMainSource] = useState(optionImages[0]);
+  const [selectedIndex, setSelectedIndex] = useState(0)
+  return (
+    <MainGalleryContainer >
+      <MainImage src={mainSource} />
+
+      <ImageOptions>
+
+        {optionImages?.map((item, index) => {
+          return (
+            <OptionContainer isSelected={selectedIndex === index} >
+              <Option onClick={() => setSelectedIndex(index)} >
+
+                <OptionImage
+                  src={item}
+                  isSelected={index === selectedIndex}
+                  onClick={(e) => setMainSource(e.target.src)}
+                />
+
+              </Option>
+            </OptionContainer>
+
+          )
+        })}
+      </ImageOptions>
+    </MainGalleryContainer>
+  );
+}
+
+
+
+
+{/* <Option >
+
+                <OptionImage
+                src={item}
+                isSelected={index === selectedIndex}
+                onClick={(e) => setMainSource(e.target.src)}
+              /> 
+
+              </Option> */}
